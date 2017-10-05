cb59110b7856f44f7791a1037bc491951f1c5400
Or Bin <orbin50@gmail.com>
2017-10-05 16:17:02 +0300

    Changed to a new configuration file and changed mail sending to Mailgun

--- fetch_and_save.py
+++ fetch_and_save.py
@@ -3,8 +3,7 @@
 import sys
 import fetch
 import logging
-import smtplib
-from email.mime.text import MIMEText
+import requests
 import json
 from datetime import datetime
 import os
@@ -13,9 +12,14 @@ from ghost import TimeoutError
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-FILENAME = "last_grades.json"
-FULL_FILE_PATH = os.path.abspath(os.path.dirname(__file__)) + "/" + FILENAME
+DATA_FILE_PATH = os.path.abspath(os.path.dirname(__file__)) + "/" + "last_grades.json"
+CONF_FILE_PATH = os.path.abspath(os.path.dirname(__file__)) + "/" + "config.json"
 FETCH_TRIES = 5
+
+prev_grades = None
+prev_timestamp = None
+curr_grades = None
+config = None
 
 logging.basicConfig(filename=os.path.abspath(os.path.dirname(__file__)) + "/" + 'log.txt',
 					filemode='a',
@@ -25,97 +29,93 @@ logging.basicConfig(filename=os.path.abspath(os.path.dirname(__file__)) + "/" + 
 
 logging.info("**************** Starting service ****************")
 logging.debug("Debug message for testing")
-file = open(os.path.abspath(os.path.dirname(__file__)) + "/" + "login_data.txt", "r")
-base_meyda_net_url = file.readline().strip()
-id_number = file.readline().strip()
-meyda_net_password = file.readline().strip()
-email_address = file.readline().strip()
-email_pass = file.readline().strip()
-file.close()
 
-prev_grades = None
-prev_timestamp = None
-curr_grades = None
-curr_timestamp = int((datetime.now() - datetime(1970,1,1)).total_seconds())
-
-# Reading previous grades from file
-if not os.path.isfile(FULL_FILE_PATH):
-	logging.info("No previous file")
+if not os.path.isfile(CONF_FILE_PATH):
+	logging.critical("No configuration file found at '%s'", CONF_FILE_PATH)
+	sys.exit(1)
 else:
-	logging.info("Reading previous file")
-	with open(FULL_FILE_PATH, "r") as f:
-		prev_data_from_file = json.loads("".join(f.readlines()))
-		prev_timestamp = prev_data_from_file[u'time']
-		prev_grades = prev_data_from_file[u'grades']
+	logging.info("Reading configuration file")
+	with open(CONF_FILE_PATH, "r") as f:
+		config = json.loads("".join(f.readlines()))
 
-# Fetching current grades
-for i in range(1, FETCH_TRIES+1):
-	try:
-		logging.info("Trying to fetch grades, attempt %s of %s" % (i, FETCH_TRIES))
-		curr_grades = fetch.fetch_grades("2017", "0", base_meyda_net_url, id_number, meyda_net_password, timeout=None)
-		break
-	except TimeoutError as timeout_err:
-		if i != FETCH_TRIES:
-			logging.info("Timeout reached. Retrying...")
-		else:
-			logging.info("Maximum attempts reached. Exiting.")
-			sys.exit(1)
+for user in config["users"]:
+	base_meyda_net_url = user["base_meyda_net_url"]
+	id_number = user["id_number"]
+	meyda_net_password = user["meyda_net_password"]
+	email_address = user["email_address"]
 
-try:
-	# Writing current grades to file
-	data = {"time": curr_timestamp, "grades": curr_grades}
-	data_json = json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
-	with open(FULL_FILE_PATH, "w") as f:
-		f.write(data_json)
+	curr_timestamp = int((datetime.now() - datetime(1970,1,1)).total_seconds())
 
-	# If there are no previous grades, there is nothing to compare to
-	if not prev_grades:
-		sys.exit(0)
-
-	# Every difference will be a tuple of ("course;semester;moed", prev_grade, curr_grade)
-	differences = []
-
-	# Add differences to the list
-	for key, curr_grade in curr_grades.iteritems():
-		ukey = key.decode("utf-8")
-		if ukey not in prev_grades.keys():
-			differences.append((ukey, None, curr_grade))
-		else:
-			if curr_grade != prev_grades[ukey]:
-				differences.append((ukey, prev_grades[ukey], curr_grade))
-
-	if len(differences) == 0:
-		logging.info("---No Changes---")
+	# Reading previous grades from file
+	if not os.path.isfile(DATA_FILE_PATH):
+		logging.info("No previous file")
 	else:
-		email_text = ""
+		logging.info("Reading previous file")
+		with open(DATA_FILE_PATH, "r") as f:
+			prev_data_from_file = json.loads("".join(f.readlines()))
+			prev_timestamp = prev_data_from_file[u'time']
+			prev_grades = prev_data_from_file[u'grades']
 
-		for diff in differences:
-			course, semester, moed = map(str, diff[0].split(";"))
-			diff = (course, semester, moed, str(diff[1]), diff[2])
+	# Fetching current grades
+	for i in range(1, FETCH_TRIES+1):
+		try:
+			logging.info("Trying to fetch grades, attempt %s of %s" % (i, FETCH_TRIES))
+			curr_grades = fetch.fetch_grades("2017", "0", base_meyda_net_url, id_number, meyda_net_password, timeout=None)
+			break
+		except TimeoutError as timeout_err:
+			if i != FETCH_TRIES:
+				logging.info("Timeout reached. Retrying...")
+			else:
+				logging.info("Maximum attempts reached. Exiting.")
+				sys.exit(1)
 
-			logging.info("קורס %s סמסטר %s מועד %s:" % (diff[0], diff[1], diff[2]))
-			logging.info("השתנה מ-%s ל-%s" % (diff[3] if diff[3] else "לא קיים", diff[4]))
+	try:
+		# Writing current grades to file
+		data = {"time": curr_timestamp, "grades": curr_grades}
+		data_json = json.dumps(data, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
+		with open(DATA_FILE_PATH, "w") as f:
+			f.write(data_json)
 
-			email_text += "הציון בקורס %s סמסטר %s מועד %s השתנה מ-%s ל-%s\n\n" % (diff[0],  # course
-																				   diff[1],  # semester
-																				   diff[2],  # moed
-																				   diff[3] if diff[
-																					   3] else "לא קיים",
-																				   # prev_grade
-																				   diff[4])  # curr_grade
+		# If there are no previous grades, there is nothing to compare to
+		if not prev_grades:
+			sys.exit(0)
 
-			msg = MIMEText(email_text)
+		# Every difference will be a tuple of ("course;semester;moed", prev_grade, curr_grade)
+		differences = []
 
-			msg['Subject'] = 'ציון חדש במידע-נט'
-			msg['From'] = email_address
-			msg['To'] = email_address
+		# Add differences to the list
+		for key, curr_grade in curr_grades.iteritems():
+			ukey = key.decode("utf-8")
+			if ukey not in prev_grades.keys():
+				differences.append((ukey, None, curr_grade))
+			else:
+				if curr_grade != prev_grades[ukey]:
+					differences.append((ukey, prev_grades[ukey], curr_grade))
 
-			s = smtplib.SMTP('smtp.gmail.com', 587)
-			s.ehlo()
-			s.starttls()
-			s.login(email_address, email_pass)
-			s.sendmail(email_address, [email_address], msg.as_string())
-			s.quit()
+		if len(differences) == 0:
+			logging.info("---No Changes---")
+		else:
+			email_text = ""
 
-except Exception as exc:
-	logging.exception(exc)
+			for diff in differences:
+				course, semester, moed = map(str, diff[0].split(";"))
+				diff = (course, semester, moed, str(diff[1]), diff[2])
+
+				logging.info("קורס %s סמסטר %s מועד %s:" % (diff[0], diff[1], diff[2]))
+				logging.info("השתנה מ-%s ל-%s" % (diff[3] if diff[3] else "לא קיים", diff[4]))
+
+				email_text += "הציון בקורס %s סמסטר %s מועד %s השתנה מ-%s ל-%s\n\n" % (diff[0],  # course
+																					   diff[1],  # semester
+																					   diff[2],  # moed
+																					   diff[3] if diff[3] else "לא קיים", # prev_grade
+																					   diff[4])  # curr_grade
+
+				res = requests.post("{}/messages".format(config["api_base_url"]),
+				                    auth=("api", config["api_key"]),
+				                    data={"from": "Meyda Net Grades Fetcher <{}>".format(config["sending_email_address"]),
+				                          "to": email_address,
+				                          "subject": "ציון חדש במידע-נט",
+				                          "text": email_text})
+
+	except Exception as exc:
+		logging.exception(exc)
